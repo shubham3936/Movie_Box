c0d70d047ded85e08bd99f0b208b563bb312916f
shubham3936 <shubham3936@gmail.com>
2022-07-01 23:22:11 +0530

    optimization of signin and signup page

--- src/components/SignInBody/SignInBody.js
+++ src/components/SignInBody/SignInBody.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import "./SignInBody.css";
+import { SIGN_IN_BODY_TEXT } from "../../constants/components";
+import { SIGN_UP_BODY_TEXT } from "../../constants/components";
 
 function SignInBody() {
-  // title
-  //         input
-  //         input
-  //         button
-  //         button
-  //         text
+
+  const [showSignInBox, setShowSignInBox] = useState(true);
+
+  function setShowSignInBoxAs(param) {
+    setShowSignInBox(param);
+  }
+
   return (
-    <div className="signinbody_container">
-      <div className="signinbody_form">
-          <p className="signinbody_title">Sign In</p>
-          <input placeholder="Email" />
-          <input placeholder="Password" />
-          <button>Sign In</button>
-          <button>Login as Guest User</button>
-          <p className="signinbody_text">New to MovieApp? <span>SignUp Now.</span></p>
-      </div>
-    </div>
+    
+    <>
+      {showSignInBox ? (
+        // SignIn Box
+        <div className="signinbody_container">
+          <div className="signinbody_form">
+            <p className="signinbody_title">{SIGN_IN_BODY_TEXT.SIGN_IN}</p>
+            <input placeholder={SIGN_IN_BODY_TEXT.EMAIL_PLACEHOLDER} />
+            <input
+              placeholder={SIGN_IN_BODY_TEXT.PASSWORD_PLACEHOLDER}
+              type="password"
+            />
+            <button>{SIGN_IN_BODY_TEXT.SIGN_IN}</button>
+            <button>{SIGN_IN_BODY_TEXT.LOGIN_AS_GUEST_USER}</button>
+            <p className="signinbody_text">
+              {SIGN_IN_BODY_TEXT.NEW_TO_MOVIE_APP}{" "}
+              <span onClick={() => setShowSignInBoxAs(false)}>
+                {SIGN_IN_BODY_TEXT.SIGN_UP_NOW}
+              </span>
+            </p>
+          </div>
+        </div>
+      ) : (
+        // SignUp Box
+        <div className="signinbody_container">
+          <div className="signinbody_form">
+            <p className="signinbody_title">{SIGN_UP_BODY_TEXT.SIGN_UP}</p>
+            <input placeholder={SIGN_UP_BODY_TEXT.NAME} />
+            <input placeholder={SIGN_UP_BODY_TEXT.EMAIL_PLACEHOLDER} />
+            <input
+              placeholder={SIGN_UP_BODY_TEXT.PASSWORD_PLACEHOLDER}
+              type="password"
+            />
+            <button>{SIGN_UP_BODY_TEXT.SIGN_UP}</button>
+            <button>{SIGN_UP_BODY_TEXT.LOGIN_AS_GUEST_USER}</button>
+            <p className="signinbody_text">
+              {SIGN_UP_BODY_TEXT.HAVE_AN_ACCOUNT}{" "}
+              <span onClick={() => setShowSignInBoxAs(true)}>{SIGN_UP_BODY_TEXT.SIGN_IN_NOW}</span>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/constants/components.js
@@ -0,0 +1,20 @@
+export const SIGN_IN_BODY_TEXT = {
+    SIGN_IN: "Sign In",
+    EMAIL_PLACEHOLDER: "Enter your email",
+    PASSWORD_PLACEHOLDER: "Enter your password",
+    LOGIN_AS_GUEST_USER: "Login as Guest User",
+    NEW_TO_MOVIE_APP: "New to MovieApp?",
+    SIGN_UP_NOW: "SignUp Now."
+}
+
+
+export const SIGN_UP_BODY_TEXT = {
+    SIGN_UP: "Sign Up",
+    NAME:"Name",
+    EMAIL_PLACEHOLDER: "Enter your email",
+    PASSWORD_PLACEHOLDER: "Enter your password",
+    LOGIN_AS_GUEST_USER: "Login as Guest User",
+    HAVE_AN_ACCOUNT: "Have an account?",
+    // NEW_TO_MOVIE_APP: "New to MovieApp?",
+    SIGN_IN_NOW: "SignIn Now."
+}
